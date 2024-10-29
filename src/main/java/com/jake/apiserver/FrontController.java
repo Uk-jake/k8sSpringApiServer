d385afa108a1db9942f172499ce5c5b003e7eee2
UkJang <mag0225@naver.com>
2024-10-29 11:31:07 +0900

    Add Dockerfile

--- src/main/java/com/jake/apiserver/FrontController.java
+++ src/main/java/com/jake/apiserver/FrontController.java
@@ -1,8 +1,30 @@
 package com.jake.apiserver;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FrontController {
-    
+    @GetMapping("/")
+    public Map<String, Object> index(){
+        Map<String, Object> data = new HashMap<>();
+        data.put("result", "success");
+
+        List<Map> list = new ArrayList<>();
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("id", "jake");
+        map1.put("name", "UkJang");
+
+        list.add(map1);
+
+        data.put("list", list);
+
+        return data;
+    }
+
 }
